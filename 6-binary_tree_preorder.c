fffb4fffb0e1df3c355c83a3c7f9626cebb0230c
Sebastion Price <sebastionprice@gmail.com>
2026-04-07 12:17:56 +1000

    Update description binary_tree_preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_preorder - traverse a binary tree in preorder
+ * @tree: root node
+ * @func: function to use on each node
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
