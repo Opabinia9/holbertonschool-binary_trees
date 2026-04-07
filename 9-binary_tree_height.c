c771bd457dd6833914e18fb72e97809acdd95a0d
Sebastion Price <sebastionprice@gmail.com>
2026-04-07 15:39:25 +1000

    Add changes not commited in last

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - wrapper for binary_proper_height
+ * @tree: root of tree
+ * Return: height of tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -10,6 +13,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	return (binary_proper_height(tree));
 }
+
+/**
+ * binary_proper_height - find height of binary tree
+ * @tree: root of tree
+ * Return: height of tree
+ */
 size_t binary_proper_height(const binary_tree_t *tree)
 {
 	size_t left, right;
@@ -19,6 +28,6 @@ size_t binary_proper_height(const binary_tree_t *tree)
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
-	
-	return ((left > right ? left : right ) + 1);
+
+	return ((left > right ? left : right) + 1);
 }
