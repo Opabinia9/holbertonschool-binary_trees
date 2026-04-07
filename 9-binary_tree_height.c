23b9894a19554b9c91fce42897139061ff7457f8
Sebastion Price <sebastionprice@gmail.com>
2026-04-07 14:20:19 +1000

    Clean binary_tree_heigh return statments

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,10 +7,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t r, l;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return 0;
+		return (0);
 
 	l = 1 + binary_tree_height(tree->left);
 	r = 1 + binary_tree_height(tree->right);
